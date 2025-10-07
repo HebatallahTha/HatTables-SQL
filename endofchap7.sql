9e26602e36cf91e53e6d23fc61ccd1ae94bfb8d6
Hebatallah Tharhan <168129594+HebatallahTha@users.noreply.github.com>
2025-10-07 10:21:19 -0400

    Update endofchap7.sql

--- endofchap7.sql
+++ endofchap7.sql
@@ -11,14 +11,36 @@ Values ('Brass');
 UPDATE hatCategories
 SET CategoryName = 'Woodwinds'
 WHERE CategoryID = 7;
-
--- OR
---UPDATE hatCategories
---SET CategoryName = 'Woodwinds'
---WHERE CategoryID = 'Brass';
 --Query 3
 DELETE hatCategories
 Where CategoryID = 7;
 SELECT * FROM hatCategories;
+--Query 4
+INSERT INTO hatProducts
+	( CategoryID, ProductCode, ProductName, Description, ListPrice, DiscountPercent, DateAdded)
+VALUES
+	( 4, 'dgx_640', 'Yamaha DGX 640 88-Key Digital Piano', 'Long description to come.', 799.99, 0, GETDATE())
+--Query 5
+UPDATE hatProducts
+SET DiscountPercent = 35
+WHERE ProductID = (SELECT MAX(ProductID) FROM hatProducts);
+--Query 6
+DELETE hatProducts
+Where CategoryID = 4;
+DELETE hatCategories
+WHERE CategoryID = 4;
+--Query 7
+INSERT INTO hatCustomers
+	(EmailAddress, Password, FirstName, LastName)
+Values
+	('rick@raven.com', '', 'Rick', 'Raven');
+--Query 8
+UPDATE hatCustomers
+Set Password = 'secret'
+Where EmailAddress = 'rick@raven.com';
+--Query 9
+UPDATE hatCustomers
+Set Password = 'reset'
+
 
 
